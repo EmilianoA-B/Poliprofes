7e636d3a06799b74d15d095bafa5d0e730a2e59e
andresarguelles <aarguellesgonzalez@gmail.com>
2024-05-29 17:43:07 -0600

    Database module reconfiguration which let us connect with 2 different passwords

--- routes/db.js
+++ routes/db.js
@@ -1,19 +1,41 @@
 const mysql = require('mysql2');
 
-//Conexion a base de datos
-const connection = mysql.createConnection({
+//Archivo de configuracion de conexion JSON
+const connectionJSON = {
     host: 'localhost',
     user: 'root',
-    password: 'Pix3l.if071102',
+    password: 'pandora',
     database: 'poliprofes'
-});
+};
 
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
+let connection;
+
+function tryConnection(connectionConfig, onSuccess, onFailure){
+    //Establecer atributo de conexion en el objeto Connection
+    connection = mysql.createConnection(connectionJSON);
+    connection.connect(err => {
+        if (err) {
+            onFailure(err);
+        }
+        console.log('Connected to the MySQL database.');
+        onSuccess(connection);
+    });
+}
+
+tryConnection(connectionJSON, 
+    (connection)=>{
+        console.log('Within the first attempt');
+    },
+    (err) =>{
+        connectionJSON.password= 'Pix3l.if071102';
+        tryConnection(connectionJSON, 
+            (connection)=>{
+                console.log('Within the second attempt');
+            },
+            (err)=>{
+                console.error('Error on the db connection: ', err.stack);
+            })
     }
-    console.log('Connected to the MySQL database.');
-});
+);
 
 module.exports = connection;
