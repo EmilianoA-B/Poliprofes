0c5832e797a074a13d9b6ada2439acd7e9039989
DRCorona21 <driverac1701@alumno.ipn.mx>
2024-05-30 12:09:31 -0600

    Co-authored-by: Emiliano Ariza Bolaños <EmilianoA-B@users.noreply.github.com> Baja de profes

--- public/scripts/dar_baja_profesor.js
+++ public/scripts/dar_baja_profesor.js
@@ -1,74 +1,67 @@
 // Variable global para almacenar una referencia al divProfesor que se debe eliminar
 var divProfesorToRemove;
 
-// Función para dar de baja al profesor
-function darDeBaja() {
-  // Verificar si divProfesorToRemove está definido y no es null
-  if (divProfesorToRemove) {
-    // Remover el divProfesor
-    divProfesorToRemove.remove();
-    // Limpiar la variable global
-    divProfesorToRemove = null;
-  }
-  cerrarModal();
-}
+
 
 // Función para mostrar el modal de confirmación
 function mostrarModal() {
-  var modal = document.getElementById("modal");
-  var overlay = document.getElementById("overlay");
-  modal.style.display = "block";
-  overlay.style.display = "block";
+  var modal = document.getElementById('modal');
+  var overlay = document.getElementById('overlay');
+  modal.style.display = 'block';
+  overlay.style.display = 'block';
 }
 
 // Función para cerrar el modal
 function cerrarModal() {
-  var modal = document.getElementById("modal");
-  var overlay = document.getElementById("overlay");
-  modal.style.display = "none";
-  overlay.style.display = "none";
+  var modal = document.getElementById('modal');
+  var overlay = document.getElementById('overlay');
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Función para mostrar los resultados en la página
   function mostrarResultados(resultados) {
-    var resultadosDiv = document.getElementById("resultados");
-    resultadosDiv.innerHTML = ""; // Limpiar resultados anteriores
+    var resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = ''; // Limpiar resultados anteriores
 
     resultados.forEach(function (profesor) {
-      var divProfesor = document.createElement("div");
-      divProfesor.classList.add("profesor-container"); //elemento div class=profesor
+      var divProfesor = document.createElement('div');
+      divProfesor.classList.add('profesor-container'); 
 
-      var enlaceNombre = document.createElement("a");
+      var enlaceNombre = document.createElement('a');
       enlaceNombre.href = `pag_prof.html?profesor=${encodeURIComponent(
         profesor.nombre
       )}`; // Enlace a la página del profesor
-      enlaceNombre.classList.add("resultado-nombre"); //elemento a class="nombre"
+      enlaceNombre.classList.add('resultado-nombre');
       enlaceNombre.innerText = profesor.nombre;
 
-      var materiasParrafo = document.createElement("p");
-      materiasParrafo.classList.add("resultado-materias"); // p class="materias"
+      var materiasParrafo = document.createElement('p');
+      materiasParrafo.classList.add('resultado-materias');
       // Agregar cada materia al párrafo en una línea separada
       profesor.materias.forEach(function (materia, index) {
         materiasParrafo.innerHTML += materia;
         if (index < profesor.materias.length - 1) {
-          materiasParrafo.innerHTML += "<br>"; // Agregar etiqueta <br> si no es la última materia
+          materiasParrafo.innerHTML += '<br>';
         }
       });
 
-      var containerButton = document.createElement("div");
-      containerButton.classList.add("container-button");
+      var containerButton = document.createElement('div');
+      containerButton.classList.add('container-button');
 
-      var button = document.createElement("button");
-      button.classList.add("baja-btn");
-      var spanTrash = document.createElement("i");
-      spanTrash.classList.add("lar", "la-trash-alt"); // Separo las clases con comas
+      var button = document.createElement('button');
+      button.classList.add('baja-btn');
+      var spanTrash = document.createElement('i');
+      spanTrash.classList.add('lar', 'la-trash-alt'); 
       button.appendChild(spanTrash);
-      button.innerHTML += " Dar de baja"; // Agrego espacio antes de "Dar de baja"
+      button.innerHTML += ' Dar de baja';
       button.onclick = function () {
         mostrarModal();
-        // Almacenar una referencia al divProfesor asociado con este botón
         divProfesorToRemove = divProfesor;
+        var nombreProf = profesor.nombre; 
+        document.getElementById('confirmarBaja').onclick = async function () {
+          await darDeBajaProfesor(nombreProf);
+        };
       };
       containerButton.appendChild(button);
       divProfesor.appendChild(enlaceNombre);
@@ -78,18 +71,44 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  // Función para dar de baja al profesor
+async function darDeBajaProfesor(nombreProf) {
+  console.log(nombreProf);
+  try {
+    const url = '/endpoint/eliminarProfesor';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nombre: nombreProf }),
+    });
+    if (!response.ok) {
+      throw new Error('Error al eliminar al profesor');
+    }
+    // Remover el divProfesor localmente después de eliminarlo en la base de datos
+    if (divProfesorToRemove) {
+      divProfesorToRemove.remove();
+      divProfesorToRemove = null;
+    }
+    cerrarModal(); // Cerrar el modal después de eliminar al profesor
+  } catch (error) {
+    console.error('Error al dar de baja al profesor:', error);
+  }
+}
+
   // Función para obtener los resultados desde el servidor
   async function obtenerResultados() {
     try {
-      let url = "/api/getProfesByCalificacionAndMaterias";
+      const url = '/api/getProfesByCalificacionAndMaterias';
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error("Error en la solicitud");
+        throw new Error('Error en la solicitud');
       }
       const resultados = await response.json();
       mostrarResultados(resultados);
     } catch (error) {
-      console.error("Error al obtener los resultados:", error);
+      console.error('Error al obtener los resultados:', error);
     }
   }
 
--- routes/endpoint.js
+++ routes/endpoint.js
@@ -1,162 +1,265 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const connection = require('./db');
+const connection = require("./db");
 
 //Registro de alumno
-router.post('/regAlumno', (req, res) => {
-    const nombre = req.body.nombre;
-    const apellidos = req.body.apellidos;
-    const correo = req.body.correo;
-    const contrasenia = req.body.contrasenia;
-    const query = 'INSERT INTO alumnos (nombre, apellidos, correo, contrasenia) VALUES (?, ?, ?, ?)'
-    connection.query(query, [nombre, apellidos, correo, contrasenia], (err, results) => {
-        /*console.log(req.body);*/ //Linea de debug
-        if (err) {
-            console.error('Error al anadir alumno', err);
-            res.status(500).json({ error: 'Error al anadir alumno', details: err });
-            return;
-        }
-        res.status(200).json({ message: 'Se anadio al usuario', userId: results.insertId });
-    });
+router.post("/regAlumno", (req, res) => {
+  const nombre = req.body.nombre;
+  const apellidos = req.body.apellidos;
+  const correo = req.body.correo;
+  const contrasenia = req.body.contrasenia;
+  const query =
+    "INSERT INTO alumnos (nombre, apellidos, correo, contrasenia) VALUES (?, ?, ?, ?)";
+  connection.query(
+    query,
+    [nombre, apellidos, correo, contrasenia],
+    (err, results) => {
+      /*console.log(req.body);*/ //Linea de debug
+      if (err) {
+        console.error("Error al anadir alumno", err);
+        res.status(500).json({ error: "Error al anadir alumno", details: err });
+        return;
+      }
+      res
+        .status(200)
+        .json({ message: "Se anadio al usuario", userId: results.insertId });
+    }
+  );
 });
 
 //Registro de carreras
-router.post('/regCarrera', (req, res) => {
-    const carrera = req.body.carrera;
-    const query = 'INSERT INTO carreras (carrera) VALUES (?)'
-    connection.query(query, [carrera], (err, results) => {
-        /*console.log(req.body);*/ //Linea de debug
-        if (err) {
-            console.error('Error anadiendo carrera', err);
-            res.status(500).json({ error: 'Error anadiendo carrera', details: err });
-            return;
-        }
-        res.status(200).json({ message: 'Se anadio la carrera', userId: results.insertId });
-    });
+router.post("/regCarrera", (req, res) => {
+  const carrera = req.body.carrera;
+  const query = "INSERT INTO carreras (carrera) VALUES (?)";
+  connection.query(query, [carrera], (err, results) => {
+    /*console.log(req.body);*/ //Linea de debug
+    if (err) {
+      console.error("Error anadiendo carrera", err);
+      res.status(500).json({ error: "Error anadiendo carrera", details: err });
+      return;
+    }
+    res
+      .status(200)
+      .json({ message: "Se anadio la carrera", userId: results.insertId });
+  });
 });
 
 //Registro de materia
-router.post('/regMateria', (req, res) => {
-    const materia = req.body.materia;
-    const carrera = req.body.ID_Carrera;
-    console.log('ID:',carrera);
-    const query = "INSERT INTO materias (materia, carrera_ID) VALUES (?, ?)";
-    connection.query(query, [materia, carrera], (err, results) => {
-        if(err){
-            console.error('Error agregando la materia', err);
-            res.status(500).send('Error agregando la materia');
-            return;
-        }
-        console.log("Registro de materia existoso");
-        res.status(200).send('Registro de materia exitoso');
-    });
+router.post("/regMateria", (req, res) => {
+  const materia = req.body.materia;
+  const carrera = req.body.ID_Carrera;
+  console.log("ID:", carrera);
+  const query = "INSERT INTO materias (materia, carrera_ID) VALUES (?, ?)";
+  connection.query(query, [materia, carrera], (err, results) => {
+    if (err) {
+      console.error("Error agregando la materia", err);
+      res.status(500).send("Error agregando la materia");
+      return;
+    }
+    console.log("Registro de materia existoso");
+    res.status(200).send("Registro de materia exitoso");
+  });
 });
 
 //Alta de profesores desde ADMIN ===> CUIDADO CON REGISTRAR REPETIDOS, SE PETA EL SERVIDOR
-router.post('/regProf', (req, res) => {
-    const nombreProf = req.body.nombreProf;
-    const apellido_p = req.body.apellidoP;
-    const apellido_m = req.body.apellidoM;
-    const todasLasMaterias = req.body.selections;
-    const verificado = req.body.verificado;
-    console.log(req.body.selections);
-    const query1 = "INSERT INTO profesores (nombre, apellido_paterno, apellido_materno, verificado) VALUES (?, ?, ?, ?)";
-    connection.query(query1, [nombreProf, apellido_p, apellido_m, verificado], (err, results) => {
-        if(err){
-            console.error('Error agregando al profesor', err);
-            res.status(500).send('Error agregando al profesor');
-            return;
-        }
-        console.log("Exito agregando al profesor");
+router.post("/regProf", (req, res) => {
+  const nombreProf = req.body.nombreProf;
+  const apellido_p = req.body.apellidoP;
+  const apellido_m = req.body.apellidoM;
+  const todasLasMaterias = req.body.selections;
+  const verificado = req.body.verificado;
+  console.log(req.body.selections);
+  const query1 =
+    "INSERT INTO profesores (nombre, apellido_paterno, apellido_materno, verificado) VALUES (?, ?, ?, ?)";
+  connection.query(
+    query1,
+    [nombreProf, apellido_p, apellido_m, verificado],
+    (err, results) => {
+      if (err) {
+        console.error("Error agregando al profesor", err);
+        res.status(500).send("Error agregando al profesor");
+        return;
+      }
+      console.log("Exito agregando al profesor");
+    }
+  );
+  //Segundo query para sacar ID de profesor
+  const query2 =
+    "SELECT id FROM profesores WHERE nombre = ? AND apellido_paterno = ? AND apellido_materno = ?";
+  connection.query(
+    query2,
+    [nombreProf, apellido_p, apellido_m],
+    (err, results) => {
+      if (results.length.id === 0) {
+        return res.status(400).send("No se encontro IDS");
+      }
+      if (err) {
+        console.error("Error al buscar id del prof", err);
+        res.status(500).send("Error al buscar id del prof");
+        return;
+      }
+      console.log("Exito encontrando el ID del profesor");
+      console.log(results);
+      const idProfesor = results[0].id;
 
-    });
-    //Segundo query para sacar ID de profesor
-    const query2 = "SELECT id FROM profesores WHERE nombre = ? AND apellido_paterno = ? AND apellido_materno = ?";
-    connection.query(query2, [nombreProf, apellido_p, apellido_m], (err, results) =>{
-        if(results.length.id === 0){
-            return res.status(400).send('No se encontro IDS');
+      //3ER query para materias
+      const materiasConComas = todasLasMaterias.map(() => "?").join(",");
+      const query3 = `SELECT id FROM materias WHERE materia IN (${materiasConComas})`;
+      connection.query(query3, todasLasMaterias, (err, results) => {
+        if (err) {
+          console.error("Error encontrando el ID de las materias", err);
+          res.status(500).send("Error encontrando el ID de las materias");
+          return;
         }
-        if(err){
-            console.error('Error al buscar id del prof', err);
-            res.status(500).send('Error al buscar id del prof');
-            return;
-        }
-        console.log("Exito encontrando el ID del profesor");
+        console.log("Exito encontrando el ID de las materias");
         console.log(results);
-        const idProfesor = results[0].id;
+        const idMaterias = results.map((row) => row.id);
+        console.log("MATERIAS ID:", idMaterias);
 
-        //3ER query para materias
-        const materiasConComas = todasLasMaterias.map(() => '?').join(',');
-        const query3 = `SELECT id FROM materias WHERE materia IN (${materiasConComas})`;
-        connection.query(query3, todasLasMaterias, (err, results) =>{
-            if(err){
-                console.error('Error encontrando el ID de las materias', err);
-                res.status(500).send('Error encontrando el ID de las materias');
-                return;
-            }
-            console.log("Exito encontrando el ID de las materias");
-            console.log(results);
-            const idMaterias = results.map(row => row.id);
-            console.log("MATERIAS ID:",idMaterias);
+        //4to query para insertar en tabla profesores_materias
+        const materiasConProf = idMaterias.map(() => "(?, ?)").join(",");
+        const valoresQuery = [];
+        idMaterias.forEach((id) => {
+          valoresQuery.push(id, idProfesor);
+        });
 
-            //4to query para insertar en tabla profesores_materias
-            const materiasConProf = idMaterias.map(() => '(?, ?)').join(',');
-            const valoresQuery = [];
-            idMaterias.forEach(id=>{
-                valoresQuery.push(id, idProfesor);
-            });
-
-            const query4 = `INSERT INTO profesor_materias (materia_id, profesor_id) VALUES ${materiasConProf}`;
-            connection.query(query4, valoresQuery, (err, results) =>{
-                if(err){
-                    console.error('Error registrando materias en tabla', err);
-                    res.status(500).send('Error registrando materias en tabla');
-                    return;
-                }
-                console.log("Exito registrando materias en tabla");
-                res.status(200).json({ message:'Se registro el profesor y sus materias con exito'});
+        const query4 = `INSERT INTO profesor_materias (materia_id, profesor_id) VALUES ${materiasConProf}`;
+        connection.query(query4, valoresQuery, (err, results) => {
+          if (err) {
+            console.error("Error registrando materias en tabla", err);
+            res.status(500).send("Error registrando materias en tabla");
+            return;
+          }
+          console.log("Exito registrando materias en tabla");
+          res
+            .status(200)
+            .json({
+              message: "Se registro el profesor y sus materias con exito",
             });
         });
-    });
+      });
+    }
+  );
 });
 
 //Eliminacion de comentario
-router.post('/eliminarComentario', (req, res) => {
-    const id_comentario = req.body.id_comentario;
-    const query = "DELETE from COMENTARIOS where ID = ?";
-    connection.query(query, [id_comentario], (err, results) => {
+router.post("/eliminarComentario", (req, res) => {
+  const id_comentario = req.body.id_comentario;
+  const query = "DELETE from COMENTARIOS where ID = ?";
+  connection.query(query, [id_comentario], (err, results) => {
+    if (err) {
+      console.error("Error eliminando el comentario", err);
+      return res
+        .status(500)
+        .json({ error: "Error eliminando el comentario", details: err });
+    }
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: "No se encontró el comentario" });
+    }
+    res.status(200).json({ message: "Comentario eliminado con éxito" });
+  });
+});
+
+// Eliminación de profesor
+router.post("/eliminarProfesor", (req, res) => {
+  const nombreCompleto = req.body.nombre;
+  console.log("Nombre desde el end:",nombreCompleto);
+  const findProfessorIdQuery = `
+    SELECT ID
+    FROM PROFESORES
+    WHERE CONCAT(NOMBRE, ' ', APELLIDO_PATERNO, ' ', APELLIDO_MATERNO) = ?
+    LIMIT 1
+`;
+
+  connection.query(findProfessorIdQuery, [nombreCompleto], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error al buscar el profesor");
+    }
+
+    if (result.length === 0) {
+      return res.status(404).send("Profesor no encontrado");
+    }
+
+    const professorId = result[0].ID;
+    console.log("ID prof:",professorId);
+
+    // SQL query to delete related records in PROFESOR_MATERIAS
+    const deleteProfesorMateriasQuery = `
+        DELETE FROM PROFESOR_MATERIAS
+        WHERE PROFESOR_ID = ?
+    `;
+
+    connection.query(deleteProfesorMateriasQuery, [professorId], (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error al eliminar materias del profesor");
+      }
+
+      // SQL query to delete related records in COMENTARIOS
+      const deleteComentariosQuery = `
+            DELETE FROM COMENTARIOS
+            WHERE PROFESORES_ID = ?
+        `;
+
+      connection.query(deleteComentariosQuery, [professorId], (err) => {
         if (err) {
-            console.error('Error eliminando el comentario', err);
-            return res.status(500).json({ error: 'Error eliminando el comentario', details: err });
+          console.error(err);
+          return res
+            .status(500)
+            .send("Error al eliminar comentarios del profesor");
         }
-        if (results.affectedRows === 0) {
-            return res.status(404).json({ message: 'No se encontró el comentario' });
-        }
-        res.status(200).json({ message: 'Comentario eliminado con éxito' });
+
+        // SQL query to delete the professor
+        const deleteProfessorQuery = `
+                DELETE FROM PROFESORES
+                WHERE ID = ?
+            `;
+
+        connection.query(deleteProfessorQuery, [professorId], (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send("Error al eliminar el profesor");
+          }
+
+          res.status(200).send("Profesor eliminado con éxito");
+        });
+      });
     });
+  });
 });
 
 //Solicitud de profesor
-router.post('/solcitarProf', (req, res) => {
-    const nombreProf = req.body.nombreProf;
-    const apellidoP = req.body.apellidoP;
-    const apellidoM = req.body.apellidoM;
-    const id_carrera = req.body.id_carrera;
-    const query1 = "INSERT INTO profesores (nombre, apellido_paterno, apellido_materno, verificado) VALUES (?, ?, ?, ?, ?)";
-    connection.query(query1, [nombreProf, apellidoP, apellidoM, 0, id_carrera], (err, results) => {
-        if (err) {
-            console.error('Error al subir solicitud de profesor', err);
-            return res.status(500).json({ error: 'Error al subir solicitud de profesor', details: err });
-        }
-        res.status(200).json({ message: 'Exito al subir solicitud de profesor' });
-    });
+router.post("/solcitarProf", (req, res) => {
+  const nombreProf = req.body.nombreProf;
+  const apellidoP = req.body.apellidoP;
+  const apellidoM = req.body.apellidoM;
+  const id_carrera = req.body.id_carrera;
+  const query1 =
+    "INSERT INTO profesores (nombre, apellido_paterno, apellido_materno, verificado) VALUES (?, ?, ?, ?, ?)";
+  connection.query(
+    query1,
+    [nombreProf, apellidoP, apellidoM, 0, id_carrera],
+    (err, results) => {
+      if (err) {
+        console.error("Error al subir solicitud de profesor", err);
+        return res
+          .status(500)
+          .json({
+            error: "Error al subir solicitud de profesor",
+            details: err,
+          });
+      }
+      res.status(200).json({ message: "Exito al subir solicitud de profesor" });
+    }
+  );
 });
 
 //Aceptar solicitud de profesor
-router.post('/aceptarProf', (req, res) =>{
-    const nombreProf = req.body.nombre;
-    const query = `UPDATE PROFESORES
+router.post("/aceptarProf", (req, res) => {
+  const nombreProf = req.body.nombre;
+  const query = `UPDATE PROFESORES
     SET VERIFICADO = TRUE
     WHERE ID = (
         SELECT ID
@@ -167,13 +270,15 @@ router.post('/aceptarProf', (req, res) =>{
             LIMIT 1
         ) AS subquery
     )`;
-    connection.query(query, [nombreProf], (err,results) => {
-        if (err) {
-            console.error('Error al aceptar prof', err);
-            return res.status(500).json({ error: 'Error al saceptar prof', details: err });
-        }
-        res.status(200).json({ message: 'Exito al aceptar prof' });
-    });
+  connection.query(query, [nombreProf], (err, results) => {
+    if (err) {
+      console.error("Error al aceptar prof", err);
+      return res
+        .status(500)
+        .json({ error: "Error al saceptar prof", details: err });
+    }
+    res.status(200).json({ message: "Exito al aceptar prof" });
+  });
 });
 
 module.exports = router;
