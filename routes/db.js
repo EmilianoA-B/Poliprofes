17e895257007cb5e9bdfac7f81e413510350ae68
DRCorona21 <driverac1701@alumno.ipn.mx>
2024-05-31 23:01:59 -0600

    Una redireccion

--- routes/db.js
+++ routes/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connectionJSON = {
     host: 'localhost',
     user: 'root',
-    password: 'pandora',
+    password: 'Pix3l.if071102',
     database: 'poliprofes'
 };
 
